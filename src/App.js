c3d4f5a9a7fad3c33f9df0a4decb3a2c92a71b72
Anthony-Oliveira <95887439+Anthony-Oliveira@users.noreply.github.com>
2022-05-05 19:54:47 -0300

    Adicionando Cartão de Crédito à Página Principal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import CreditCard from "./Components/CreditCard";
+import CardList from "./Components/CardList";
+import CenteredButton from "./Components/CenteredButton";
 import "./Views/App.scss";
 
 const App = () => (
   <div className="App">
     <Navbar />
+    <Hero />
+
+    <CreditCard />
   </div>
 );
 
